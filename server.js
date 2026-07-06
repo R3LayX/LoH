ba86b55a6362c214be2beaba52290bc88f0b3723
R3LayX <R3LayX@users.noreply.github.com>
2026-07-06 17:16:22 +0000

    Full site: 4 pages with Janice's story, Shopify links, contact form

--- server.js
+++ server.js
@@ -7,6 +7,23 @@ const PORT = 3000;
 // Serve static files from the current directory
 app.use(express.static(__dirname));
 
+// Serve specific pages
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/letters', (req, res) => {
+  res.sendFile(path.join(__dirname, 'letters.html'));
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'about.html'));
+});
+
+app.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, 'contact.html'));
+});
+
 // Fall through to index.html for SPA-like navigation (Express 5 syntax)
 app.get('/{*path}', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
